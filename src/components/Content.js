c193951eea79dbafca5bbd5b0afdc4f0076529b2
xiaominzhu <xiaominzhu@Freeman.local>
2020-10-06 17:56:27 +0200

    Updated render content

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,30 +3,37 @@ import Info from "./Info.js";
 import { useHistory, Link, Switch, Route } from "react-router-dom";
 
 const Content = ({ content }) => {
+  const [type, setType] = React.useState(0);
   const history = useHistory();
 
   return (
     <div>
       <ul>
-        {content.map((item, i) => {
-          return (
-            <li
-              key={i}
-              onClick={() => {
-                history.push(`/posts/${item._id}`);
-              }}
-            >
-              <Link to={`/posts/${item._id}`}>
-                Title:{item.title} Text: {item.text}
-              </Link>
-              <Switch>
-                <Route path={`/posts/${item._id}`}>
-                  <Info content={content} />
-                </Route>
-              </Switch>
-            </li>
-          );
-        })}
+        {type === 0 ? (
+          content.map((item, i) => {
+            const path = `/posts/${item._id}`;
+            return (
+              <li
+                key={i}
+                onClick={() => {
+                  history.push(path);
+                  setType(1);
+                }}
+              >
+                <Link to={path}>
+                  Title:{item.title} Text: {item.text}
+                </Link>
+                <Switch>
+                  <Route path={path}>
+                    <Info content={content} />
+                  </Route>
+                </Switch>
+              </li>
+            );
+          })
+        ) : (
+          <Info content={content} />
+        )}
       </ul>
     </div>
   );
